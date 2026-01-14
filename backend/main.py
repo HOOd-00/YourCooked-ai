b3b50163fd8b80b17edb29ff6bd90de3d23638fa
overphu <ramsiripooh@gmail.com>
2026-01-14 12:55:22 +0700

    update: setting service url for frontend & backend deployment

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Frontend origin
+    allow_origins=["https://yourcooked-ai.vercel.app", "http://localhost:3000"], # http://localhost:3000 Frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
